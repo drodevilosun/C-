90ff893609505da5c6de81ce1512c88cfa55cb6c
drodevilosun <drodevilosun@gmail.com>
2018-04-09 15:36:44 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright(C) 2015 - 2016 Renesas Electronics Corporation.                   *
+ * All Rights Reserved.                                                        *
+ *                                                                             *
+ ******************************************************************************/
+/*
+ * This is practical source code while learning CPP of FA team
+ */
+#include <stdio.h>
+
+#include "task1.h"
+#include "task2.h"
+#include "task3.h"
+#include "task4.h"
+#include "task5.h"
+
+int main(int argc, char* argv[]) {
+	printf("Hello from FA team \n");
+
+	// call tasks
+	if(1) Task_1 a;
+	if(1) Task_2 a;
+	if(1) Task_3 a;
+	if(1) Task_4 a;
+	if(1) Task_5 a;
+
+	return 0;
+}
