9cd231a5ef8d54776dde0252fb825eb2b8acb397
drodevilosun <drodevilosun@gmail.com>
2018-04-10 14:29:05 +0700

    Create Investigate.c

---
+++ Investigate.c
@@ -0,0 +1,3 @@
+https://www.geeksforgeeks.org/when-are-constructors-called/
+https://www.geeksforgeeks.org/memory-layout-of-c-program/
+http://vietjack.com/cplusplus/constructor_va_destructor_trong_cplusplus.jsp
