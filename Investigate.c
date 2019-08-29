1bb9cd2f55813661a50c5722fa0f923f4a2be5bb
drodevilosun <drodevilosun@gmail.com>
2019-08-29 21:43:50 +0700

    Update Investigate.c

--- Investigate.c
+++ Investigate.c
@@ -2,3 +2,4 @@ https://www.geeksforgeeks.org/when-are-constructors-called/
 https://www.geeksforgeeks.org/memory-layout-of-c-program/
 http://vietjack.com/cplusplus/constructor_va_destructor_trong_cplusplus.jsp
 Test fetch
+Test fetch 2
