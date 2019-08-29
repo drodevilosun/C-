a3bbd690861002a21ca2763e5763935717105310
Andrew Max <drodevilosun@gmail.com>
2019-08-29 22:20:49 +0700

    Test char pointer point to int data.

--- KHTN/W03/rule_off_tree2/main.cpp
+++ KHTN/W03/rule_off_tree2/main.cpp
@@ -6,6 +6,8 @@ using namespace std;
 class Animal {
 private:
 	int price;
+	int *data;
+	int *data1;
 public:
 	int price2;
 	Animal() {
@@ -13,10 +15,27 @@ public:
 	}
 	Animal(int tmp) {
 		price = tmp;
+		data = new int(tmp);
+		data1 = new int[10]();
 		cout << "Base class is called with pv value:" << price << endl;
+	}
+	Animal(const Animal & An) {
+		data = new int;
+		*data = An.data[0];
+		price = An.price;
 	}
 	void func_f() {
 		cout << "Base class call function f" << endl;
+	}
+	void set_data(int num) {
+		*data = num;
+	}
+	void print_data() {
+		cout << "data is: " << *data << endl;
+	}
+	~Animal() {
+		delete data;
+		delete []data1;
 	}
 };
 class Teo :public Animal {
@@ -40,20 +59,32 @@ void Function_X(Animal *tt) {
 int main() {
 	Teo *Long = new Teo(10);
 	Function_X(Long);
+	
+	cout << endl << endl;
+	Animal x(3);
+	Animal y(x);
+	y.set_data(4);
+	x.print_data();
+	y.print_data();
 
+	cout << endl << endl;
 
 	/* Test pointer */
 	//int a[10] = { 0 , 1, 2 ,3, 4, 5, 6, 7, 8 , 9 };
-	char a[10] = { 200 , 1000, '0' ,'0', '4', '5', '6', '7', '8' , '9' };
+	char a[10] = { '0' , '1', '2' ,'3', '4', '5', '6', '7', '8' , '9' };
+	int *t = new int[10]();
 	int i;
+	for (i = 0; i < 10; i++) {
+		cout << t[i] << " ";
+	}
 	for (i = 0; i < 10; i++) {
 		cout << a[i] << endl;
 	}
 
-	/*int *s;
-	s = (int *)a;*/
-	short *s;
-	s = (short *)a;
+	int *s;
+	s = (int *)(a + 3);
+	/*short *s;
+	s = (short *)a;*/
 
 
 	//s = (int*) (a + 2);
