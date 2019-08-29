4dd8ada39bfc22a3dc0e05fbb6996fab777e249e
drodevilosun <drodevilosun@gmail.com>
2019-08-29 22:26:19 +0700

    Release test fetch 1

--- Investigate.c
+++ Investigate.c
@@ -1,3 +1,4 @@
 https://www.geeksforgeeks.org/when-are-constructors-called/
 https://www.geeksforgeeks.org/memory-layout-of-c-program/
 http://vietjack.com/cplusplus/constructor_va_destructor_trong_cplusplus.jsp
+Test fetch 1 in Release branch
